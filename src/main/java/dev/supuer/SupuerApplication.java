b837cf8f2ed31e476760331f45c8f6edd0f8cb10
jun <alap_u@naver.com>
2023-10-21 00:32:43 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/dev/supuer/SupuerApplication.java
@@ -0,0 +1,13 @@
+package dev.supuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupuerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupuerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/supuer/SupuerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.supuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupuerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
